87c70f1eb92f19b724fc84e3d7f9c72805c94a44
JunSeop Kim <wnqtjq8958@naver.com>
2024-05-22 17:40:18 +0900

    0522 실습과제

---
+++ 0522/work_space_nodejs_kb5/Q1.index1.js
@@ -0,0 +1,6 @@
+const users = require("./Q1.user1.js");
+const hello = require("./Q1.hello.js");
+
+
+console.log(users);
+hello(users.user3);
---
+++ 0522/work_space_nodejs_kb5/Q1.index2.js
@@ -0,0 +1,6 @@
+const person = require("./Q1.user2")
+
+console.log(person);
+console.log('name >> ', person.name);
+console.log('age >> ', person.age);
+console.log('tel >> ', person.tel);
