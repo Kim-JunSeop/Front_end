205ba357324657c172800161c6f3264945507da6
JunSeop Kim <wnqtjq8958@naver.com>
2024-05-27 16:41:24 +0900

    0527 실습예제

---
+++ 0524/work_space_front_vue_kb5/5.vue_sfc_basic/vue-sfc-1-app/src/components/CheckBoxItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <li>
+        <input type="checkbox" v-model="checked">
+        <span class="test">옵션1</span>
+    </li>
+</template>
+<script>
+export default{
+    name:'CheckBoxItem', 
+    data(){
+        return{
+            checked:false,
+
+        };
+    }
+}
+</script>
+<style scoped>
+    .test{
+        color:blue;
+    }
+</style>
